42909ce757cd6b42eac7c47d975f9c57c6f03233
Bezruchenko Alexandr <handless@live.ru>
2013-08-01 22:35:27 +0300

    First Commit of this project
    This program will shutdown your computer after 5 minutes

---
+++ ShutDownTimer/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace ShutDownTimer
+{
+    partial class ShutDownTimer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(49, 81);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(75, 23);
+            this.buttonRun.TabIndex = 0;
+            this.buttonRun.Text = "Выключить";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(46, 22);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(109, 13);
+            this.labelInfo.TabIndex = 1;
+            this.labelInfo.Text = "Осталось времени: ";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // ShutDownTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(258, 124);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.buttonRun);
+            this.Name = "ShutDownTimer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ShutDownTimer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ ShutDownTimer/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace ShutDownTimer
+{
+    public partial class ShutDownTimer : Form
+    {
+        DateTime ShutDownTime;
+        public ShutDownTimer()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            // Присваеваем атрибуту текущее время + 5 минут
+            ShutDownTime = DateTime.Now.AddMinutes(5);
+
+            // Запускаем наш таймер
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (ShutDownTime > DateTime.Now)
+            {
+                // Высчитываем отрезок времени (Время выключения - Текущее время) 
+                TimeSpan ts = ShutDownTime - DateTime.Now;
+                // Выводим на наш label который у меня называется labelInfo
+                labelInfo.Text = "Осталось времени: " + ts.Minutes + " min " +
+                    ts.Seconds + " sec";
+            }
+            else 
+            {
+               //Вызываем процесс shutdown который собственно и выключит наш компьютер.
+                Process.Start("shutdown", "/s /t 0");
+                Close();
+            }
+
+        }
+    }
+}
